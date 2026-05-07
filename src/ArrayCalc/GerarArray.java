22d1053a839b95d7ec82198b91e9a020c48e95de
PietroBIN <peterguidorizzi@gmail.com>
2026-05-07 15:10:17 -0300

    Criada a Class GerarArray com tamanho e range de números escolhidos.

--- src/ArrayCalc/GerarArray.java
+++ src/ArrayCalc/GerarArray.java
@@ -4,14 +4,14 @@ package ArrayCalc;
 import java.util.Random;
 
 public class GerarArray {
-    public static int[] Gerador() {
+    public static int[] Gerador(int tamanho, int range) {
         Random random = new Random();
-        int[] numerous = new int[random.nextInt(1, 100)];
+        int[] numeros = new int[tamanho];
 
-        for (int i = 0; i < numerous.length; i++) {
-            numerous[i] = random.nextInt(100);
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = random.nextInt(range);
         }
 
-        return numerous;
+        return numeros;
     }
 }
