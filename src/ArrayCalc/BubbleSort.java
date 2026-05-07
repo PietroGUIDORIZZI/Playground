5b9d41ebe2bd3901904b32460cc60a3092156d7c
PietroBIN <peterguidorizzi@gmail.com>
2026-05-07 17:17:07 -0300

    Classe BubbleSort Otimizada.

--- src/ArrayCalc/BubbleSort.java
+++ src/ArrayCalc/BubbleSort.java
@@ -4,8 +4,8 @@ public class BubbleSort {
     public static int[] bubbleSort(int[] array) {
         int temp = 0;
         int i = 0;
-        for (i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
+        for (i = 0; i < array.length -1; i++) {
+            for (int j = 0; j < array.length - 1-i; j++) {
                 if (array[j] > array[j + 1]) {
                     temp = array[j];
                     array[j] = array[j+1];
